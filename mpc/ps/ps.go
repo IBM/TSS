6144dc013310ecbdc04974768a420706e8ff776b
Yacov Manevich <yacov.manevich@ibm.com>
2023-09-14 16:20:11 +0200

    Implementing vector version of PS
    Signed-off-by: Yacov Manevich <yacov.manevich@ibm.com>

--- mpc/ps/ps.go
+++ mpc/ps/ps.go
@@ -9,156 +9,244 @@ package ps
 import (
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
 	math "github.com/IBM/mathlib"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
-	"io"
 )
 
-// This is an implementation algorithm 6.1 from the paper:
-// "Short Randomizable Signatures"
-// By David Pointcheval and Olivier Sanders
-// https://eprint.iacr.org/2015/525.pdf
+// This is an implementation of a threshold blind signature scheme and zero knowledge proof of knowledge of such signature.
+// The notation follows appendix C from "Privacy-preserving auditable token payments in a permissioned blockchain system" - https://eprint.iacr.org/2019/1058.pdf
+// but the actual algorithm closely follows Appendix B from
+// the paper: "Coconut: Threshold Issuance Selective Disclosure  Credentials with Applications to Distributed Ledgers" - https://arxiv.org/pdf/1802.07344.pdf
+// with a slight deviation where we add an extra message m' as in section 4.2 in "Reassessing Security of Randomizable Signatures" - https://eprint.iacr.org/2017/1197.pdf
 
-var (
-	c     = math.Curves[1]
-	g1    = c.HashToG1([]byte("PS G1"))
-	g2    = psuedoRandomG2()
-	unity = c.GenG1.Copy().Mul(c.GroupOrder)
-)
+type PP struct {
+	c  *math.Curve
+	g2 *math.G2
+	g0 *math.G1
+	g  *math.G1
+	gs []*math.G1
+	n  int // |m|+1
+}
 
-type SK math.G1
-
-func (sk SK) Sign(rand io.Reader, blindedM *math.G1) BlindSignature {
-	u := c.NewRandomZr(rand)
-
-	XC := (math.G1)(sk)
-	XC.Add(blindedM)
-
-	return BlindSignature{
-		R: g1.Mul(u),
-		S: XC.Mul(u),
+func Setup(c *math.Curve, messageLength int) PP {
+	return PP{
+		n:  messageLength + 1,
+		c:  c,
+		g2: psuedoRandomG2(c),
+		g0: psuedoRandomG1s(c, 1, "G0")[0],
+		g:  psuedoRandomG1s(c, 1, "G")[0],
+		gs: psuedoRandomG1s(c, messageLength+1, "Gs"),
 	}
+}
+
+type SK struct {
+	x  *math.Zr
+	ys []*math.Zr
 }
 
 type PK struct {
-	Y1     math.G1
-	X2, Y2 math.G2
+	X  *math.G2
+	Ys []*math.G2
 }
 
-type Signature struct {
-	R *math.G1
-	S *math.G1
-}
-
-func (s *Signature) Verify(pk *PK, m *math.Zr) error {
-	if s.R.Equals(unity) {
-		return fmt.Errorf("σ1 is 1")
+func LocalKeyGen(pp PP) (SK, PK) {
+	sk := SK{
+		ys: make([]*math.Zr, pp.n+1),
 	}
 
-	XYm := pk.X2.Copy()
-	XYm.Add((&pk.Y2).Mul(m))
-
-	left := c.Pairing(XYm, s.R)
-	c.FExp(left)
-
-	right := c.Pairing(g2, s.S)
-	c.FExp(right)
-
-	if left.Equals(right) {
-		return nil
+	sk.x = pp.c.NewRandomZr(rand.Reader)
+	for i := 0; i < len(sk.ys); i++ {
+		sk.ys[i] = pp.c.NewRandomZr(rand.Reader)
 	}
 
-	return fmt.Errorf("invalid signature")
+	pk := PK{
+		X: pp.g2.Mul(sk.x),
+	}
+
+	for i := 0; i < len(sk.ys); i++ {
+		pk.Ys[i] = pp.g2.Mul(sk.ys[i])
+	}
+
+	return sk, pk
 }
 
-type BlindSignature struct {
-	R *math.G1
-	S *math.G1
+func Blind(pp *PP, c *math.Curve, m []*math.Zr) {
+	if len(m)+1 != pp.n {
+		panic(fmt.Sprintf("expected message to be of length %d but was of length %d", pp.n-1, len(m)))
+	}
+	rcm := c.NewRandomZr(rand.Reader)
+
+	// private key for ElGamal
+	z := c.NewRandomZr(rand.Reader)
+
+	// public key for ElGamal
+	u := pp.g.Mul(z)
+
+	// commitment to 'm'
+	cm := commit(pp, rcm, m)
+
+	mPrime := pp.c.HashToZr(hash(cm.Bytes()))
+
+	// Add m' to commitment
+	cm.Add(pp.gs[len(m)].Mul(mPrime))
+
+	// Update 'h' with final commitment
+	h := pp.c.HashToG1(cm.Bytes())
+
+	msg := make([]*math.Zr, pp.n)
+	copy(msg, m)
+	msg[len(msg)-1] = mPrime
+	a, b, r := encrypt(pp, c, msg, h, u)
+
+	_ = a
+
+	// Last element of b vector is encryption of m'
+	lastBi := h.Mul(mPrime)
+	lastBi.Add(u.Mul(r[len(r)-1]))
+	b[len(b)-1] = lastBi
 }
 
-func (bs *BlindSignature) UnBlind(blindingFactor *math.Zr) Signature {
-	S := bs.S.Copy()
-	S.Add(neg(bs.R.Mul(blindingFactor)))
+func commit(pp *PP, rcm *math.Zr, m []*math.Zr) *math.G1 {
+	cm := pp.g0.Mul(rcm)
+	for i := 0; i < len(m); i++ {
+		cm.Add(pp.gs[i].Mul(m[i]))
+	}
+	return cm
+}
 
-	return Signature{
-		R: bs.R,
-		S: S,
+func encrypt(pp *PP, c *math.Curve, m []*math.Zr, h *math.G1, u *math.G1) ([]*math.G1, []*math.G1, []*math.Zr) {
+	r := make([]*math.Zr, len(m))
+	for i := 0; i < len(r); i++ {
+		r[i] = c.NewRandomZr(rand.Reader)
+	}
+
+	a := make([]*math.G1, len(m))
+	for i := 0; i < len(a); i++ {
+		a[i] = pp.g.Mul(r[i])
+	}
+
+	b := make([]*math.G1, len(m))
+	for i := 0; i < len(m); i++ {
+		bi := h.Mul(m[i])
+		bi.Add(u.Mul(r[i]))
+		b[i] = bi
+	}
+	return a, b, r
+}
+
+func proveBlindingIsWellFormed(c *math.Curve, m []*math.Zr, r []*math.Zr, a, b []*math.G1, rcm *math.Zr, g, g0, h, u, cm *math.G1, gs []*math.G1) BlindCorrectFormProof {
+	n := len(m)
+
+	α := make([]*math.Zr, n)
+	for i := 0; i < n; i++ {
+		α[i] = c.NewRandomZr(rand.Reader)
+	}
+
+	β := make([]*math.Zr, n)
+	for i := 0; i < n; i++ {
+		β[i] = c.NewRandomZr(rand.Reader)
+	}
+
+	γ := c.NewRandomZr(rand.Reader)
+
+	d := make([]*math.G1, n)
+	f := make([]*math.G1, n)
+
+	s := g0.Mul(γ)
+	for i := 0; i < n; i++ {
+		s.Add(gs[i].Mul(β[i]))
+		d[i] = h.Mul(β[i])
+		d[i].Add(u.Mul(α[i]))
+
+		f[i] = g.Mul(α[i])
+	}
+
+	digest := randomOracleForBlindingProof(n, d, f, s, a, b, cm, g, g0, h, u, gs)
+	e := c.HashToZr(digest)
+
+	z := γ.Plus(e.Mul(rcm))
+	x := make([]*math.Zr, n)
+	y := make([]*math.Zr, n)
+	for i := 0; i < n; i++ {
+		x[i] = α[i].Plus(e.Mul(r[i]))
+		y[i] = β[i].Plus(e.Mul(m[i]))
+	}
+
+	return BlindCorrectFormProof{
+		x: x,
+		y: y,
+		s: s,
+		f: f,
+		d: d,
+		z: z,
 	}
 }
 
-func Gen() (SK, PK) {
-	x, y := c.NewRandomZr(rand.Reader), c.NewRandomZr(rand.Reader)
-	X1, Y1 := g1.Mul(x), g1.Mul(y)
-	X2, Y2 := g2.Mul(x), g2.Mul(y)
-	return SK(*X1), PK{Y1: *Y1, X2: *X2, Y2: *Y2}
+func randomOracleForBlindingProof(n int, d []*math.G1, f []*math.G1, s *math.G1, a, b []*math.G1, cm *math.G1, g *math.G1, g0 *math.G1, h *math.G1, u *math.G1, gs []*math.G1) []byte {
+	hash := sha256.New()
+	for i := 0; i < n; i++ {
+		hash.Write(d[i].Bytes())
+		hash.Write(f[i].Bytes())
+		hash.Write(a[i].Bytes())
+		hash.Write(b[i].Bytes())
+	}
+	hash.Write(s.Bytes())
+	hash.Write(cm.Bytes())
+
+	hash.Write(g.Bytes())
+	hash.Write(g0.Bytes())
+	hash.Write(h.Bytes())
+	hash.Write(u.Bytes())
+	for i := 0; i < n; i++ {
+		gs[i].Bytes()
+	}
+	digest := hash.Sum(nil)
+	return digest
 }
 
-func Blind(pk *PK, rand io.Reader, m *math.Zr) (*math.G1, *math.Zr, ProofOfOpening) {
-	t := c.NewRandomZr(rand)
-	C := g1.Mul(t)
-	C.Add(pk.Y1.Mul(m))
-	com := commitment{x: m, r: t}
-	return C, t, com.prove(rand, &pk.Y1, g1)
+type BlindCorrectFormProof struct {
+	x, y []*math.Zr
+	s    *math.G1
+	z    *math.Zr
+	d, f []*math.G1
 }
 
-type commitment struct {
-	x, r *math.Zr
-}
+func (ξ BlindCorrectFormProof) Verify(c *math.Curve, n int, a, b []*math.G1, cm *math.G1, g *math.G1, g0 *math.G1, h *math.G1, u *math.G1, gs []*math.G1) error {
+	digest := randomOracleForBlindingProof(n, ξ.d, ξ.f, ξ.s, a, b, cm, g, g0, h, u, gs)
+	e := c.HashToZr(digest)
 
-type ProofOfOpening struct {
-	U, V *math.Zr
-	D    *math.G1
-}
-
-func (pop ProofOfOpening) Verify(com, g, h *math.G1) error {
-	e := HVZKChallenge(pop.D, g, h)
-	r := com.Mul(e)
-	r.Add(pop.D)
-
-	l := g.Mul(pop.U)
-	l.Add(h.Mul(pop.V))
-
-	if l.Equals(r) {
-		return nil
+	for i := 0; i < n; i++ {
+		left := u.Mul(ξ.x[i])
+		left.Add(h.Mul(ξ.y[i]))
+		right := ξ.d[i]
+		right.Add(b[i].Mul(e))
+		if !left.Equals(right) {
+			return fmt.Errorf("u^{x%d}h^{y%d} != d%db%d^e", i, i, i, i)
+		}
 	}
 
-	return fmt.Errorf("invalid proof of opening")
-}
-
-func (com commitment) prove(rand io.Reader, g, h *math.G1) ProofOfOpening {
-	y, s := c.NewRandomZr(rand), c.NewRandomZr(rand)
-	d1 := g.Mul(y)
-	d2 := h.Mul(s)
-	d := d1
-	d.Add(d2)
-
-	e := HVZKChallenge(d, g, h)
-
-	u := y.Plus(e.Mul(com.x))
-	v := s.Plus(e.Mul(com.r))
-
-	return ProofOfOpening{
-		U: u,
-		V: v,
-		D: d,
+	for i := 0; i < n; i++ {
+		left := g.Mul(ξ.x[i])
+		right := ξ.f[i].Copy()
+		right.Add(a[i].Mul(e))
+		if !left.Equals(right) {
+			return fmt.Errorf("g^{x%d} != f%da%d^e", i, i, i)
+		}
 	}
 
-}
+	left := cm.Mul(e)
+	left.Add(ξ.s)
+	right := g0.Mul(ξ.z)
+	for i := 0; i < n; i++ {
+		right.Add(gs[i].Mul(ξ.y[i]))
+	}
+	if !left.Equals(right) {
+		return fmt.Errorf("cm^e*s != g0^z * \\prod{gi^yi}")
+	}
 
-func HVZKChallenge(d, g, h *math.G1) *math.Zr {
-	// Public parameters and 'd' go into the random oracle
-
-	gBytes := g.Bytes()
-	hBytes := h.Bytes()
-	dBytes := d.Bytes()
-
-	preimage := make([]byte, 0, len(gBytes)+len(hBytes)+len(dBytes))
-	preimage = append(preimage, gBytes...)
-	preimage = append(preimage, hBytes...)
-	preimage = append(preimage, dBytes...)
-
-	return c.HashToZr(hash(preimage))
+	return nil
 }
 
 func hash(in []byte) []byte {
@@ -167,7 +255,21 @@ func hash(in []byte) []byte {
 	return h.Sum(nil)
 }
 
-func psuedoRandomG2() *math.G2 {
+func psuedoRandomG1s(c *math.Curve, count int, label string) []*math.G1 {
+	res := make([]*math.G1, count)
+	for i := 0; i < count; i++ {
+		buff := make([]byte, 0, 20)
+		buff = append(buff, []byte("PS")...)
+		buff = append(buff, []byte(label)...)
+		n := make([]byte, 2)
+		binary.BigEndian.PutUint16(n, uint16(i))
+		buff = append(buff, n...)
+		res[i] = c.HashToG1(hash(buff))
+	}
+	return res
+}
+
+func psuedoRandomG2(c *math.Curve) *math.G2 {
 	g2, err := bn254.HashToG2([]byte("PS"), []byte("G2"))
 	if err != nil {
 		panic(err)
@@ -182,6 +284,7 @@ func psuedoRandomG2() *math.G2 {
 	return g
 }
 
+/*
 func neg(in *math.G1) *math.G1 {
 	zero := c.GenG1.Copy()
 	zero.Sub(zero)
@@ -189,3 +292,4 @@ func neg(in *math.G1) *math.G1 {
 	zero.Sub(in)
 	return zero
 }
+*/
--- mpc/ps/ps_test.go
+++ mpc/ps/ps_test.go
@@ -8,20 +8,37 @@ package ps
 
 import (
 	"crypto/rand"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	math "github.com/IBM/mathlib"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestPS(t *testing.T) {
-	sk, pk := Gen()
-	m := c.NewRandomZr(rand.Reader)
+func TestBlindCorrectFormProof(t *testing.T) {
+	c := math.Curves[1]
 
-	blindedMsg, blindingFactor, proof := Blind(&pk, rand.Reader, m)
-	err := proof.Verify(blindedMsg, &pk.Y1, g1)
-	assert.NoError(t, err)
+	msgLen := 10
 
-	blindedSig := sk.Sign(rand.Reader, blindedMsg)
-	signature := blindedSig.UnBlind(blindingFactor)
-	err = signature.Verify(&pk, m)
+	pp := Setup(c, msgLen)
+	//sk, pk := LocalKeyGen(pp)
+
+	msg := make([]*math.Zr, msgLen)
+	for i := 0; i < msgLen; i++ {
+		msg[i] = c.NewRandomZr(rand.Reader)
+	}
+
+	hExp := c.NewRandomZr(rand.Reader)
+	h := pp.g.Mul(hExp)
+
+	z := c.NewRandomZr(rand.Reader)
+	u := pp.g.Mul(z)
+
+	a, b, r := encrypt(&pp, c, msg, h, u)
+
+	rcm := c.NewRandomZr(rand.Reader)
+	cm := commit(&pp, rcm, msg)
+
+	ξ := proveBlindingIsWellFormed(c, msg, r, a, b, rcm, pp.g, pp.g0, h, u, cm, pp.gs)
+	err := ξ.Verify(c, len(msg), a, b, cm, pp.g, pp.g0, h, u, pp.gs)
 	assert.NoError(t, err)
 }
